f16d812ee22fb0f158f1bd8ae4a83026463dc83f
Ann-c-Tseng <ann.c.tseng@gmail.com>
2021-10-11 19:41:10 -0700

    generated the bubble sort arrays

---
+++ BubbleSort/script.js
@@ -0,0 +1,32 @@
+var container = document.getElementById("array");
+  
+// Function to generate the array of blocks
+function generateArray() {
+    for (var i = 0; i < 20; i++) {
+  
+        // Return a value from 1 to 100 (both inclusive)
+        var value = Math.ceil(Math.random() * 100);
+  
+        // Creating element div
+        var array_ele = document.createElement("div");
+  
+        // Adding class 'block' to div
+        array_ele.classList.add("block");
+  
+        // Adding style to div
+        array_ele.style.height = `${value * 3}px`;
+        array_ele.style.transform = `translate(${i * 30}px)`;
+  
+        // Creating label element for displaying 
+        // size of particular block
+        var array_ele_label = document.createElement("label");
+        array_ele_label.classList.add("block_id");
+        array_ele_label.innerText = value;
+  
+        // Appending created elements to index.html 
+        array_ele.appendChild(array_ele_label);
+        container.appendChild(array_ele);
+    }
+}
+
+generateArray();
